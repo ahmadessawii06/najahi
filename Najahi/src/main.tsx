494ac6bcf60bdf90d76521537d50534af2182b9c
yaqeenashour <ashouryaqen@gmail.com>
2025-12-18 00:59:54 +0200

    Add Login page

--- Najahi/src/main.tsx
+++ Najahi/src/main.tsx
@@ -3,12 +3,10 @@ import App from './components/header'  /*check the path twice */
 import '/src/index.css'
 import RenderFooter from './components/footer'
 import { StrictMode } from 'react'
-
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         
     <App pageName="home" />
-
     <RenderFooter pageName="home" />
     </StrictMode>
 )
