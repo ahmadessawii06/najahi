d17e0c078e65903be4d601ac7800f5d3d6878fe0
ahmadessawii06 <ahmadessawii06@gmail.com>
2025-12-19 20:10:07 +0200

    Adding Logo In The Header

--- Najahi/src/components/header.tsx
+++ Najahi/src/components/header.tsx
@@ -1,4 +1,3 @@
-
 import { Link, NavLink } from "react-router-dom";
 function Header() {
   return (
@@ -11,15 +10,37 @@ function Header() {
 
         <nav>
           <ul className="nav-links">
-            <li><NavLink to="/login">تسجيل الدخول</NavLink></li>
-            <li><NavLink to="/main">الصفحة الرئيسية</NavLink></li>
-            <li><NavLink to="/about">من نحن</NavLink></li>
-            <li><NavLink to="/donation">التبرع بالمواد</NavLink></li>
-            <li><NavLink to="/search">البحث عن الكتب</NavLink></li>
-            <li><NavLink to="/uni">الجامعات الفلسطينية</NavLink></li>
-            <li><NavLink to = "/groups">مجموعات الدراسة</NavLink></li>
-            <li><NavLink to = "/teachers">المعلمون القريبون</NavLink></li>
-            <li><NavLink to = "/syntag"><img src="../public/Icons/SynTag-WithoutBG.png" className="syntag" /></NavLink></li>
+            <li>
+              <NavLink to="/login">تسجيل الدخول</NavLink>
+            </li>
+            <li>
+              <NavLink to="/main">الصفحة الرئيسية</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">من نحن</NavLink>
+            </li>
+            <li>
+              <NavLink to="/donation">التبرع بالمواد</NavLink>
+            </li>
+            <li>
+              <NavLink to="/search">البحث عن الكتب</NavLink>
+            </li>
+            <li>
+              <NavLink to="/uni">الجامعات الفلسطينية</NavLink>
+            </li>
+            <li>
+              <NavLink to="/groups">مجموعات الدراسة</NavLink>
+            </li>
+            <li>
+              <NavLink to="/teachers">المعلمون القريبون</NavLink>
+            </li>
+            <li>
+              <NavLink to="/syntag" className="syntag"> 
+                <span className="Syn">Syn</span>
+                <span className="Tag">Tag</span>
+            
+              </NavLink>
+            </li>
           </ul>
         </nav>
       </div>
