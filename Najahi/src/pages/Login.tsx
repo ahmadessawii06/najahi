95202e1bb1912ba8e8f6f7065ca0afed2a2a5262
LeenArafat <leenarafat789@gmail.com>
2025-12-19 21:31:58 +0200

    solve conflicts again

--- Najahi/src/pages/Login.tsx
+++ Najahi/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'; 
+
 function Login() {
   const [activeForm, setActiveForm] = useState<'login' | 'signup' | 'forgot'>('login');
   
--- Najahi/src/pages/Main.tsx
+++
@@ -1,8 +0,0 @@
-
-function Main() {
-  return (
-    <h1>Type Here Code Main</h1>
-  )
-}
-
-export default Main
