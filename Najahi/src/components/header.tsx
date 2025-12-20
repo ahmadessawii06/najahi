4a68dc4327e79735182e8e63d3449398817ec8e3
ahmadessawii06 <ahmadessawii06@gmail.com>
2025-12-20 21:34:11 +0200

    Fix Header

--- Najahi/src/components/header.tsx
+++ Najahi/src/components/header.tsx
@@ -1,5 +1,55 @@
+// import { Link, NavLink } from "react-router-dom";
+// function Header() {
+//   return (
+//     <header>
+//       <div className="header-container">
+//         <Link to="/login" className="logo">
+//           <i className="fas fa-graduation-cap"></i>
+//           <span>نجاحي</span>
+//         </Link>
+
+//         <nav>
+//           <ul className="nav-links">
+//             <li> <NavLink to="/login">تسجيل الدخول</NavLink>  </li>
+//             <li> <NavLink to="/home">الصفحة الرئيسية</NavLink> </li> {/*This shoud be home, not main */}
+//             <li> <NavLink to="/about">من نحن</NavLink> </li>
+//             <li> <NavLink to="/donation">التبرع بالمواد</NavLink> </li>
+//             <li> <NavLink to="/search">البحث عن الكتب</NavLink>  </li>
+//             <li> <NavLink to="/uni">الجامعات الفلسطينية</NavLink> </li>
+//             <li> <NavLink to="/groups">مجموعات الدراسة</NavLink> </li>
+//             <li> <NavLink to="/teachers">المعلمون القريبون</NavLink> </li>
+//             <li>
+//               <NavLink to="/syntag" className="syntag"> 
+//                 <span className="Syn">Syn</span>
+//                 <span className="Tag">Tag</span>
+            
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+
+
+import { useState } from 'react';
 import { Link, NavLink } from "react-router-dom";
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
       <div className="header-container">
@@ -9,20 +59,24 @@ function Header() {
         </Link>
 
         <nav>
-          <ul className="nav-links">
-            <li> <NavLink to="/login">تسجيل الدخول</NavLink>  </li>
-            <li> <NavLink to="/home">الصفحة الرئيسية</NavLink> </li> {/*This shoud be home, not main */}
-            <li> <NavLink to="/about">من نحن</NavLink> </li>
-            <li> <NavLink to="/donation">التبرع بالمواد</NavLink> </li>
-            <li> <NavLink to="/search">البحث عن الكتب</NavLink>  </li>
-            <li> <NavLink to="/uni">الجامعات الفلسطينية</NavLink> </li>
-            <li> <NavLink to="/groups">مجموعات الدراسة</NavLink> </li>
-            <li> <NavLink to="/teachers">المعلمون القريبون</NavLink> </li>
+          {/* زر القائمة (الثلاث نقاط) - يظهر فقط على الشاشات الصغيرة */}
+          <button className="nav-toggle" onClick={toggleMenu}>
+            <i className="fas fa-bars"></i>
+          </button>
+
+          <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
+            <li> <NavLink to="/login" onClick={closeMenu}>تسجيل الدخول</NavLink>  </li>
+            <li> <NavLink to="/home" onClick={closeMenu}>الصفحة الرئيسية</NavLink> </li>
+            <li> <NavLink to="/about" onClick={closeMenu}>من نحن</NavLink> </li>
+            <li> <NavLink to="/donation" onClick={closeMenu}>التبرع بالمواد</NavLink> </li>
+            <li> <NavLink to="/search" onClick={closeMenu}>البحث عن الكتب</NavLink>  </li>
+            <li> <NavLink to="/uni" onClick={closeMenu}>الجامعات الفلسطينية</NavLink> </li>
+            <li> <NavLink to="/groups" onClick={closeMenu}>مجموعات الدراسة</NavLink> </li>
+            <li> <NavLink to="/teachers" onClick={closeMenu}>المعلمون القريبون</NavLink> </li>
             <li>
-              <NavLink to="/syntag" className="syntag"> 
+              <NavLink to="/syntag" className="syntag" onClick={closeMenu}> 
                 <span className="Syn">Syn</span>
                 <span className="Tag">Tag</span>
-            
               </NavLink>
             </li>
           </ul>
